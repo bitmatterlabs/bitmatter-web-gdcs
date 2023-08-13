567b4b3dac129dbbabe3051b8ddc0ea9e4a5cbb2
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-08-13 15:34:40 +0530

    feat: init nextjs starter project

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontSize: {
+        'main-heading': ["6rem", "1"],
+      },
+      spacing: {
+        0: "0",
+        1: "0.4rem",
+        2: "0.8rem",
+        3: "1.2rem",
+        4: "1.6rem",
+        5: "2.4rem",
+        6: "3.2rem",
+        7: "4rem",
+        8: "4.8rem",
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
