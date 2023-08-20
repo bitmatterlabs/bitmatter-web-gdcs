bd832f9b67d342f20cc075f86d3641ea1ea0b99a
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-08-20 09:15:20 +0530

    feat: minor SEO optimization fixes

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -2,7 +2,10 @@ import { MetadataRoute } from "next";
 
 const githubActionsWorkflow = {
   isActionRunning: process.env.GITHUB_ACTIONS || false,
-  repo: process.env.GITHUB_REPOSITORY,
+  repo: process.env.GITHUB_REPOSITORY?.toString().replace(
+    new RegExp(`^${process.env.GITHUB_REPOSITORY_OWNER || ""}`),
+    ""
+  ),
   repoOwner: process.env.GITHUB_REPOSITORY_OWNER,
 };
 
